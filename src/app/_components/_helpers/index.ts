feeb747eb94e9815403d301b1047e98e1048f21f
jude785 <velosojudekhalil@gmail.com>
2026-05-17 21:15:38 +0800

    fake backend is working

--- src/app/_components/_helpers/index.ts
+++
@@ -1,4 +0,0 @@
-export * from './app.initializer';
-export * from './error.interceptor';
-export * from './jwt.interceptor';
-export * from './must-match.validator';
--- src/app/account/register.component.ts
+++ src/app/account/register.component.ts
@@ -61,3 +61,4 @@ export class RegisterComponent implements OnInit {
                 }
             });
     }
+}
